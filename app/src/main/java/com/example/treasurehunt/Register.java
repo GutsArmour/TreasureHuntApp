bb692997d829a0903bc81be1b418f4054e0a616b
GutsArmour <ahmedizhan90@gmail.com>
2022-12-02 13:36:54 +0000

    Created login system

---
+++ app/src/main/java/com/example/treasurehunt/Register.java
@@ -0,0 +1,82 @@
+package com.example.treasurehunt;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Register extends AppCompatActivity {
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://treasurehunt-bb6e9-default-rtdb.firebaseio.com/");
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText fullname = findViewById(R.id.fullname);
+        final EditText username = findViewById(R.id.username);
+        final EditText email = findViewById(R.id.email);
+        final EditText password = findViewById(R.id.password);
+        final EditText conPassword = findViewById(R.id.conPassword);
+
+        final Button registerBtn = findViewById(R.id.registerBtn);
+        final TextView loginNowBtn = findViewById(R.id.loginNowBtn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String fullnameTxt = fullname.getText().toString();
+                final String usernameTxt = username.getText().toString();
+                final String emailTxt = email.getText().toString();
+                final String passwordTxt = password.getText().toString();
+                final String conPasswordTxt = conPassword.getText().toString();
+
+                if (fullnameTxt.isEmpty() || usernameTxt.isEmpty() || emailTxt.isEmpty() || passwordTxt.isEmpty() || conPasswordTxt.isEmpty()) {
+                    Toast.makeText(Register.this, "Please fill in all the fields", Toast.LENGTH_SHORT).show();
+                }
+                else if (!passwordTxt.equals(conPasswordTxt)){
+                    Toast.makeText(Register.this, "Entered Passwords don't match", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.hasChild(usernameTxt)) {
+                                Toast.makeText(Register.this, "Email is already registered", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                databaseReference.child("users").child(usernameTxt).child("fullname").setValue(fullnameTxt);
+                                databaseReference.child("users").child(usernameTxt).child("email").setValue(emailTxt);
+                                databaseReference.child("users").child(usernameTxt).child("password").setValue(passwordTxt);
+
+                                Toast.makeText(Register.this, "User Registered successfully", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+        });
+        loginNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
