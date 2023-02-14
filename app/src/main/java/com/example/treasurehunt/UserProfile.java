cfc570f1a1cd1acd46d962a430244e635df4a350
GutsArmour <ahmedizhan90@gmail.com>
2023-02-14 20:22:08 +0000

    Change pfp and store in firebase

--- app/src/main/java/com/example/treasurehunt/UserProfile.java
+++ app/src/main/java/com/example/treasurehunt/UserProfile.java
@@ -5,22 +5,42 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class UserProfile extends BaseAppCompatActivity{
+
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://treasurehunt-bb6e9-default-rtdb.firebaseio.com/");
+    StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://treasurehunt-bb6e9.appspot.com");
+    ImageView userPfp;
+    FirebaseAuth mAuth = Firebase.mAuth;
+    Uri pfpUri;
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +48,26 @@ public class UserProfile extends BaseAppCompatActivity{
         setContentView(R.layout.activity_userprofile);
         ImageButton homeBtn = findViewById(R.id.homeBtn);
 
-        ImageView userPfp = findViewById(R.id.userprofileImage);
+        userPfp = findViewById(R.id.userprofileImage);
         TextView userName = findViewById(R.id.userprofileUsername);
         TextView userFullname = findViewById(R.id.userprofileFullname);
         TextView userEmail = findViewById(R.id.userprofileEmail);
         TextView userPoints = findViewById(R.id.userprofilePoints);
+        Button saveProfile = findViewById(R.id.saveProfileBtn);
+
+        userPfp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gallery.launch("image/*");
+            }
+        });
+
+        saveProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadPfp();
+            }
+        });
 
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +76,7 @@ public class UserProfile extends BaseAppCompatActivity{
             }
         });
         SharedPreferences sharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE);
+        username = sharedPreferences.getString("username", "default value");
         boolean darkModeActive = sharedPreferences.getBoolean("darkModeActive", false);
 
         if (darkModeActive) {
@@ -58,20 +94,41 @@ public class UserProfile extends BaseAppCompatActivity{
                     userName.setText("Username: " + username);
                     userFullname.setText("Full Name: " + snapshot.child(username).child("fullname").getValue(String.class));
                     userEmail.setText("Email: " + snapshot.child(username).child("email").getValue(String.class));
-                    String points = snapshot.child(username).child("points").getValue(Long.class).toString();
-                    userPoints.setText("Points: " + points);
-                    String imagePfp = snapshot.child(username).child("pfp").getValue(String.class);
-                    Glide.with(UserProfile.this).load(imagePfp).into(userPfp);
-
-                    //databaseReference.child("users").child(username).child("scan_history").child(qrCodeValue).child("timestamp").setValue(timestamp);
-                    //String qrCodeKey = databaseReference.child("users").child(username).child("scan_history").push().getKey();
-                    //databaseReference.child("users").child(username).child("scan_history").child(qrCodeKey).child("timestamp").setValue(timestamp);
+                    userPoints.setText("Points: " + snapshot.child(username).child("points").getValue(Long.class).toString());
+                    Glide.with(UserProfile.this).load(snapshot.child(username).child("pfp").getValue(String.class)).into(userPfp);
                 }
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
+    }
+    ActivityResultLauncher<String> gallery = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+        @Override
+        public void onActivityResult(Uri result) {
+            if (result != null) {
+                userPfp.setImageURI(result);
+                pfpUri = result;
+            }
+        }
+    });
+    private void uploadPfp() {
+        if (pfpUri != null) {
+            StorageReference pfpRef = storageReference.child("pfps/" + UUID.randomUUID().toString());
+            pfpRef.putFile(pfpUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(UserProfile.this, "Image uploaded successfully", Toast.LENGTH_SHORT).show();
+                        pfpRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                databaseReference.child("users").child(username).child("pfp").setValue(uri.toString());
+                            }
+                        });
+                    }
+                }
+            });
+        }
     }
 }
